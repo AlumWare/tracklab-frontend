e78c6c14501a38abea90907eaf2a95c41aded3b0
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-07-07 13:26:59 -0500

    ahora si

--- src/features/tracking/views/test.component.vue
+++ src/features/tracking/views/test.component.vue
@@ -1,11 +1,13 @@
 <template>
-    <button @click="open = true">Escanear QR</button>
-    <qr-scanner v-model:open="open" @scanned="handleScanned" />
+    <div>
+      <p>Componente de prueba</p>
+    </div>
   </template>
   <script setup>
   import { ref } from 'vue'
-  import QrScanner from '@/components/qr-scanner.component.vue'
+  
   const open = ref(false)
+  
   function handleScanned(id) {
     // Aquí recibes el id numérico del QR
     console.log('QR escaneado:', id)
